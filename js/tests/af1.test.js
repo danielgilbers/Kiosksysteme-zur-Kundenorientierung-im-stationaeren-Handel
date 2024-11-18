68b61ea8e5cb78e7c4b963ba180bf4733841f140
Daniel Gilbers <a1b977b>
2024-11-18 17:39:10 +0000

    refactor

--- js/tests/af1.test.js
+++ js/tests/af1.test.js
@@ -10,23 +10,19 @@ import { initializeMap } from '../map'
 describe('Akzeptanztest AF1: 2D-Karte toom Zollstock', () => {
   let mapContainer
 
-  beforeEach(() => {
+  beforeAll(() => {
     // HTML-Setup für die Karte
     document.body.innerHTML = `
       <div class="d-flex flex-column h-100">
         <div class="flex-grow-1" id="map"></div>
       </div>`
     mapContainer = document.getElementById('map')
-  })
 
-  afterEach(() => {
-    jest.clearAllMocks()
-    document.body.innerHTML = '' // Aufräumen des DOMs
+    // Initialisiere die Karte
+    initializeMap()
   })
 
   test('Leaflet wird initialisiert und Karte angezeigt', () => {
-    // Initialisiere die Karte
-    initializeMap()
     // Überprüfe, ob das Map-Element existiert
     expect(mapContainer).toBeTruthy()
     expect(mapContainer.classList.contains('leaflet-container')).toBe(true)
@@ -40,21 +36,5 @@ describe('Akzeptanztest AF1: 2D-Karte toom Zollstock', () => {
     expect(overlayElement).toBeTruthy()
     expect(overlayElement.tagName).toBe('IMG')
     expect(overlayElement.src).toContain('/map/Zollstock-Modell.png')
-  })
-
-  test('Leaflet-Methoden aufrufen', () => {
-    // Spionieren auf Leaflet-Methoden
-    const mapSpy = jest.spyOn(L, 'map')
-    const tileLayerSpy = jest.spyOn(L, 'imageOverlay')
-
-    // Karte erneut initialisieren
-    initializeMap()
-
-    // Sicherstellen, dass die Leaflet-Methoden aufgerufen wurden
-    expect(mapSpy).toHaveBeenCalledTimes(1)
-    expect(tileLayerSpy).toHaveBeenCalled()
-
-    mapSpy.mockRestore()
-    tileLayerSpy.mockRestore()
   })
 })
